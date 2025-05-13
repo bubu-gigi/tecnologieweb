6b6bd14c6d118aea3dc079d220559576f8e39e38
Guglielmo Borgognoni <guglielmo.borgognoni@grupposcai.it>
2025-05-13 15:46:39 +0200

    fix button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
 @props(['class' => '', 'type' => ''])
-<button type={{ $type }} class="bg-blue-500 text-white px-4 py-2 rounded-lg w-max cursor-pointer {{ $class }}">
+<button type="{{ $type }}" class="text-white px-4 py-2 rounded-lg w-max cursor-pointer {{ $class }}">
     {{ $slot }}
 </button>
