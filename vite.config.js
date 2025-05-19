4481098a7580c70f4f7ad63d677915b66123c17d
Guglielmo Borgognoni <guglielmo.borgognoni@grupposcai.it>
2025-05-19 12:08:48 +0200

    vite config refresh false

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,7 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
             ],
-            refresh: [
-                // Rimuoviamo refresh automatico da storage/
-                'resources/views/**/*.blade.php',
-                'resources/js/**/*.js',
-                'resources/css/**/*.css',
-                'routes/**/*.php',
-            ],
+            refresh: false,
         }),
         tailwindcss(),
     ],
