fe3bf7aeebbfcd60aea223e293a5fcc23ff444a7
DanX242 <daniel.ibraj@davincidegiorgio.it>
2025-05-28 12:23:01 +0200

    navbar_customer inclusa nella vista customer

--- resources/views/layouts/layout_customer.blade.php
+++ resources/views/layouts/layout_customer.blade.php
@@ -9,37 +9,14 @@
 </head>
 <body>
     <x-header>
-        <x-wrapper-navbar>
-            <x-left-navbar>
-                <x-link-navbar href="/">
-                    Home
-                </x-link-navbar>
-                <x-link-navbar href="/customers">
-                    Dashboard
-                </x-link-navbar>
-                <x-link-navbar href="/customers/profilo">
-                    Profilo
-                </x-link-navbar>
-                <x-link-navbar href="/customers/prestazioni">
-                    Prestazioni
-                </x-link-navbar>
-                <x-link-navbar href="/customers/prenotazioni">
-                    Prenotazioni
-                </x-link-navbar>
-            </x-left-navbar>
-            <x-right-navbar>
-                <x-button-navbar href="/logout">
-                    Logout
-                </x-button-navbar>
-            </x-right-navbar>
-        </x-wrapper-navbar>
+        @include('layouts.navbar_customer')
     </x-header>
 
     <main class="main-content">
         @yield('content')
     </main>
 
-    @extends('layouts.footer')
+    @extends('layouts.footer') <!-- perchè qua extends e non include? che cambia? -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     @stack('scripts')
 </body>
