9390bd712661a4b352628ccd533599468be425ef
manufara <emafaraglia@hotmail.com>
2025-06-05 16:44:25 +0200

    Update statistiche.blade.php

--- resources/views/admin/statistiche.blade.php
+++ resources/views/admin/statistiche.blade.php
@@ -14,7 +14,7 @@
             <div>
                 <label for="data_inizio" class="block text-gray-700">Data Inizio</label>
                 <input type="date" name="data_inizio" id="data_inizio" 
-                    value="{{ request('data_fine') ?? '2025-06-01' }}"
+                    value="{{ request('data_inizio') ?? '2025-06-01' }}"
                    class="w-full border border-gray-300 rounded px-3 py-2 focus:ring-indigo-400 focus:outline-none">
             </div>
 
