9497710c92892225a7a5889369f4f1be35d2af9c
DanX242 <daniel.ibraj@davincidegiorgio.it>
2025-06-07 21:19:18 +0200

    prova ma non va...

--- resources/views/staff/prenotazioni_prestazione.blade.php
+++ resources/views/staff/prenotazioni_prestazione.blade.php
@@ -4,8 +4,7 @@
 
 @section('content')
 <x-card class="bg-white p-4 rounded-lg shadow-md">
-    @if(isset($prenotazioni) && count($prenotazioni) > 0)
-    <form method="GET">
+    @if(isset($prenotazioni) && count($prenotazioni) > 0)    
         <div class="flex justify-between items-center mb-2">
             <p class="text-base font-semibold text-indigo-700 mr-4">Prenotazioni {{ $prenotazioni->first()->prestazione->descrizione }}</p>
             <x-input
@@ -15,17 +14,19 @@
                 min="2025-06-01"
                 max="2025-06-30"
             />
-            <x-button class="ml-2 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold">
+
+            <x-button 
+                class="ml-2 bg-indigo-600 hover:bg-indigo-700 text-white font-semibold"
+                id="bottone_reset"
+                >
                 Azzera
             </x-button>
         </div>
-    </form>
-    <x-table :headers="['Utente', 'Data e Ora Prenotazione']">
+    <x-table id="tabella_prenotazioni" :headers="['Utente', 'Data e Ora Prenotazione']">
         @foreach($prenotazioni as $prenotazione)
             <tr class="hover:bg-indigo-50 transition">
                 <td class="px-6 py-3 capitalize">{{ $prenotazione->user->cognome }} {{ $prenotazione->user->nome }}</td>
                 <td class="px-6 py-3">{{ $prenotazione->data_prenotazione }}</td>
-                
             </tr>
         @endforeach
     </x-table>
@@ -34,3 +35,23 @@
     @endif
 </x-card>
 @endsection
+
+@push('script')
+<script>
+        const date = document.getElementById('data_prenotazione');
+        const table_rows = document.querySelectorAll('tr');
+
+        date.addEventListener('input', searchTable);
+
+        function searchTable() {
+            const search_data = date.value;
+            table_rows.forEach(row => {
+                const cellDateTime = row.cells[1].innerText.trim(); 
+                const [rowDate] = cellDateTime.split(' '); 
+
+                row.classList.toggle('hidden', rowDate !== search_data);
+            });
+        }
+</script>
+@endpush
+
