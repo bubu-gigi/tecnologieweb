0451fd9bfde8102809ea5a1c9327deb90f6d9f5c
CactusGiack <giacomomarini23@gmail.com>
2025-10-03 10:15:34 +0200

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-slate-200 text-slate-700 py-6 text-center">
     <p>
-        Azienda Tecnologica S.r.l. - Via della Commercio 123, Milano<br>
+        Azienda Tecnologica S.r.l. - Via del Commercio 123, Milano<br>
         Tel. 0123 456789 | info@aziendatecnologica.it
     </p>
 </footer>
