c88b076879a9106062e431bae1e12aa4f70b74b3
CactusGiack <giacomomarini23@gmail.com>
2025-10-03 10:33:56 +0200

    navbar arancione

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,4 +1,5 @@
-<header class="bg-indigo-700 text-white px-8 py-8 flex justify-between items-center rounded-b-lg">
+<header class="bg-[#FB7116] text-white px-8 py-8 flex justify-between items-center rounded-b-lg">
     {{ $slot }}
     <img src="{{ asset('images/logo.jpg') }}" alt="Logo" class="h-20 w-20 object-contain rounded-lg">
 </header>
+
--- resources/views/layouts/layout_guest.blade.php
+++ resources/views/layouts/layout_guest.blade.php
@@ -9,9 +9,7 @@
 </head>
 <body>
     <x-header class="bg-[#FB7116]">
-        <div class="max-w-[1200px] mx-auto px-6 py-6">
             @include('layouts.navbar_guest')
-        </div>
     </x-header>
 
     <main class="flex-1 px-8 max-w-[1000px] mx-auto">
