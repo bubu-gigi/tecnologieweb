62c73dd87eb908ef765505a7923b8730e3e47a5f
CactusGiack <giacomomarini23@gmail.com>
2025-10-11 17:10:45 +0200

    modifica grafica navbar

--- resources/views/layouts/navbar_amministratore.blade.php
+++ resources/views/layouts/navbar_amministratore.blade.php
@@ -1,6 +1,6 @@
 <section class="bg-white px-8 py-4 border border-[#FB7116] flex justify-between items-center flex-wrap gap-4 rounded-lg">
     <nav class="flex items-center justify-end gap-4 px-6 py-3">
-        <a href="{{ route('amministratore.dashboard') }}" class="text-[#FB7116] hover:text-orange-600">
+        <a href="{{ route('amministratore.dashboard') }}" class="px-4 py-2 border-2 border-[#FB7116] text-[#FB7116] font-semibold rounded-md hover:bg-[#FB7116] hover:text-white transition">
             Home
         </a>
         <a href="{{ route('logout') }}" class="px-4 py-2 border-2 border-[#FB7116] text-[#FB7116] font-semibold rounded-md hover:bg-[#FB7116] hover:text-white transition">
